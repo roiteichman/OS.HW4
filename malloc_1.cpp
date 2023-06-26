07142cfba7f4ddfeb998374a4b35e8734438cc4c
teich <roi.teichman@campus.technion.ac.il>
2023-06-26 17:54:16 +0300

    smalloc

---
+++ malloc_1.cpp
@@ -0,0 +1,16 @@
+//
+// Created by teich on 26/06/2023.
+//
+
+#define MAXSIZE 100000000
+
+void* smalloc(size_t size){
+    if(size == 0 || size > MAXSIZE ){
+        return NULL;
+    }
+    void* result = sbrk(size);
+    if(result!=(void*)(-1)){
+        return result;
+    }
+    return NULL;
+}
