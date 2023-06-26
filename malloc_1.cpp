4f73109ca2245249d12e9c9fed12333eef02b890
teich <roi.teichman@campus.technion.ac.il>
2023-06-26 18:28:19 +0300

    smalloc

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -21,8 +21,10 @@ int main(){
     void* a = smalloc(0);
     void* b = smalloc(100000001);
     void* c = smalloc(100000000);
+    void* d = smalloc(99999999);
     printf("a: %u\n", (unsigned int*)a);
     printf("b: %u\n", (unsigned int*)b);
     printf("c: %u\n", (unsigned int*)c);
+    printf("d: %u\n", (unsigned int*)d);
     return 0;
 }
