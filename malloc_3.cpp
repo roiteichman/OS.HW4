caa64538edd03de37e78c1a418fc7101fd616df5
teich <roi.teichman@campus.technion.ac.il>
2023-07-05 19:36:00 +0300

    sfree if freed already

--- malloc_3.cpp
+++ malloc_3.cpp
@@ -514,6 +514,11 @@ void sfree(void* p){
     // else - free means get the right address and update flag
     MallocMetadata* to_free = (MallocMetadata*)p - 1;
 
+    if (to_free->is_free){
+        // dont need to free p again - so return
+        return;
+    }
+
     safety(to_free);
 
     to_free->is_free= true;
